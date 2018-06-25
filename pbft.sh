e13c1f7427b6b05f2222a5b54707134c369afcaf
Bridger Herman <bridger@bitwise.io>
2018-06-25 12:01:28 -0500

    Add single-node test for PBFT
    Signed-off-by: Bridger Herman <bridger@bitwise.io>

--- pbft.sh
+++ pbft.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-docker-compose -f tests/pbft_client.yaml up;
-docker-compose -f tests/pbft_client.yaml down
+if [ $# -eq 1 ]
+then
+  docker-compose -f tests/pbft_single.yaml up;
+  docker-compose -f tests/pbft_single.yaml down
+else
+  docker-compose -f tests/pbft_client.yaml up;
+  docker-compose -f tests/pbft_client.yaml down
+fi
