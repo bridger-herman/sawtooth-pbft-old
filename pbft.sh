b4e8bee1f5bfa909e72f2e731709f2b6d9ad165a
Bridger Herman <bridger@bitwise.io>
2018-06-27 09:56:12 -0500

    Add 2 node PBFT test
    Signed-off-by: Bridger Herman <bridger@bitwise.io>

--- pbft.sh
+++ pbft.sh
@@ -4,6 +4,10 @@ if [ $# -eq 1 ]
 then
   docker-compose -f tests/pbft_single.yaml up;
   docker-compose -f tests/pbft_single.yaml down
+elif [ $# -eq 2 ]
+then
+  docker-compose -f tests/pbft_double.yaml up;
+  docker-compose -f tests/pbft_double.yaml down
 else
   docker-compose -f tests/pbft_client.yaml up;
   docker-compose -f tests/pbft_client.yaml down
