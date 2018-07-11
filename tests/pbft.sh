f7015a6f98df1ea6689a8a57521a47c8d7fef453
Bridger Herman <bridger@bitwise.io>
2018-07-11 15:07:16 -0500

    Update testing files
    Signed-off-by: Bridger Herman <bridger@bitwise.io>

---
+++ tests/pbft.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source .env
+
+DOCKER_TEST=$SAWTOOTH_CORE/integration/sawtooth_integration/docker/test_pbft_engine.yaml
+
+# docker-compose -f $DOCKER_TEST up;
+docker-compose -f $DOCKER_TEST up --abort-on-container-exit --exit-code-from test-pbft-engine;
+docker-compose -f $DOCKER_TEST down
